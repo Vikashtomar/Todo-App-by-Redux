dbf9a05839f882e1bc0e8810247c78fe31dff50b
vikash <vikash.tomar.india@gmail.com>
2023-09-09 22:10:07 +0530

    update

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -2,22 +2,22 @@ import React,{useState} from 'react'
 import { useDispatch } from 'react-redux'
 import {addTodo} from '../Features/todo/todoSlice'
  
-
 function AddTodo() {
     const [input,setInput] = useState('')
     const dispatch = useDispatch()
     const addTodoHandler = (e) =>{
-        e.preventDefault()
+        // e.preventDefault()
         dispatch(addTodo(input))
-        setInput('')
+        setInput("")
     }
 
   return ( 
-    <div>
+    <div className='mainheader'>
 
         <form onSubmit={addTodoHandler}>
-            <input type="text" placeholder='here is to do'  value={input} onChange={(e)=> setInput(e.target.value)}/>
+            <input type="text" placeholder='Add your todo'  value={input} onChange={(e)=> setInput(e.target.value)}/>
             <button type='submit'>Add</button>
+            <button>remove</button>
         </form>
     </div>
   )
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -8,18 +8,18 @@ function Todos() {
    const todos =  useSelector((state) => state.todos)
     console.log(todos)
     console.log(todos.text)
-const dispatch = useDispatch()
+// const dispatch = useDispatch()
 
   return (
     <>
     <div>Todos</div>
-    
+  
     {todos.map((todo)=>{
         return(
            <ul>
             <li>
                 {todo.text}
-                <button onClick={()=> dispatch(removeTodo(todo.id))}>Remove</button>
+                <button onClick={()=> useDispatch(removeTodo(todo.id))}>Remove</button>
             </li>
 
           
